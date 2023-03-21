150b0e965b96870f528d14664f40e96715a4502b
pbaekgaard <pbaekgaard.studie@gmail.com>
2023-03-21 14:22:27 +0100

    yes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace vBadCompiler
+{
+    public class Scanner
+    {
+
+    }
+}
