714edbfe78826b4d55a9d1f3775c515234bb6029
pbaekgaard <ZW57DP@student.aau.dk>
2023-05-07 19:51:57 +0200

    Added test for while loop

--- vGutCompiler.Tests/CodeGenerationOutputsVBA.cs
+++ vGutCompiler.Tests/CodeGenerationOutputsVBA.cs
@@ -7,11 +7,6 @@ namespace Unit_Tests
 {
     public class CodeGenerationOutputsVBA
     {
-        private readonly ITestOutputHelper output;
-        public CodeGenerationOutputsVBA(ITestOutputHelper output)
-        {
-            this.output = output;
-        }
         [Fact]
         public void CodeGeneratorOutputsCorrectVBA()
         {
@@ -23,10 +18,47 @@ namespace Unit_Tests
             var visitor = new Visitors();
             visitor.Visit(parser.program());
 
-            var exceptedOutput = "Dim A2 As Double\nA2 = 10.0\nDim A3 As Boolean\nA3 = True\n";
+            var expectedOutput = "Dim A2 As Double\nA2 = 10.0\nDim A3 As Boolean\nA3 = True\n";
             string actualOutput = visitor.codeG.Code;
             //ASSERT
-            Assert.Equal(exceptedOutput, actualOutput);
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void CodeGenerationProducesCorrectIfStatements()
+        {
+            // ARRANGE
+            GLexer lexer = new GLexer(new AntlrInputStream(@"number A2 = 10\n number A1 = 5\n if A2 > A1 then A2 = 20 endif"));
+
+            // ACT
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            GrammarParser parser = new GrammarParser(tokens);
+            var visitor = new Visitors();
+            visitor.Visit(parser.program());
+
+            var expectedOutput = "Dim A2 As Double\nA2 = 10.0\nDim A1 As Double\nA1 = 5.0\nIf A2 > A1 Then A2 = 20";
+            string actualOutput = visitor.codeG.Code;
+
+            //ASSERT
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void CodeGenerationProducesCorrectWhileLoops()
+        {
+            GLexer lexer = new GLexer(new AntlrInputStream(@"number A2 = 10\n while A2 == 10\n A2 = 11 endwhile"));
+
+            // ACT
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            GrammarParser parser = new GrammarParser(tokens);
+            var visitor = new Visitors();
+            visitor.Visit(parser.program());
+
+            var expectedOutput = "Dim A2 As Double\nA2 = 10\nDo While A2 == 10\nA2 = 11\nLoop";
+            string actualOutput = visitor.codeG.Code;
+
+            //ASSERT
+            Assert.Equal(expectedOutput, actualOutput);
         }
     }
 }
