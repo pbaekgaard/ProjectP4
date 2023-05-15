632a3bef14fb905c76f0319bba56bbdcc5129e5a
pbaekgaard <pbaekgaard.studie@gmail.com>
2023-05-15 15:08:00 +0200

    Unit tests for Code Generation added

--- vGutCompiler.Tests/Unit.cs
+++ vGutCompiler.Tests/Unit.cs
@@ -2,6 +2,10 @@ using Antlr4.Runtime;
 using Xunit.Abstractions;
 using vGutCompiler;
 using ProjectP4;
+using Moq;
+using Antlr4.Runtime.Tree;
+using System.Reflection.Metadata.Ecma335;
+using Xunit.Sdk;
 
 namespace Unit
 {
@@ -78,6 +82,35 @@ namespace Unit
             // ASSERT
             var exceptionUpdate = Assert.Throws<Exception>(() => SymbolTable.updateSymbol("MissingSymbol", testSymbol));
             Assert.Equal(exceptionUpdate.Message, "MissingSymbol is not declared");
+        }
+
+        [Fact]
+        public void CodeGeneration_GeneratesCorrectVLOOKUP()
+        {
+            //ARRANGE
+            CodeGenerator CodeGen = new();
+
+            // ACT
+            CodeGen.VLookUpFunction(10, "A1", "B4", 2, false);
+            string actual = CodeGen.Code;
+
+            string expected = "WorksheetFunction.VLOOKUP(10, Range(\"A1:B4\"), 2, False)";
+            // ASSERT
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void CodeGeneration_GeneratesCorrectSORT()
+        {
+            //ARRANGE
+            CodeGenerator CodeGen = new();
+
+            // ACT
+            CodeGen.SortFunction("A1", "A4", "B2", "true");
+            string actual = CodeGen.Code;
+
+            string expected = "Range(\"A1:A4\").Copy Destination:=Range(\"B2\")\nRange(\"B2:B5\").Sort Key1:=Range(\"B2\"), Order1:=xlAscending, Header:=xlNo\n";
+            // ASSERT
+            Assert.Equal(expected, actual);
         }
     }
 }
